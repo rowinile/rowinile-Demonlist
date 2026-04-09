623950539a421de171d036fa382aeffd46dd26c4
rowinile <fbberatgok757@gmail.com>
2026-04-09 13:46:14 +0300

    bug fixes
    Updated the displayLevel function to include a clickable link for the completion video and improved HTML structure for displaying level details.

--- js/constructors.js
+++ js/constructors.js
@@ -21,13 +21,13 @@ const levels = [
     new Level("Starlight Summit by hazelmrow", "https://youtu.be/1hbvPzbMG3Q", "11", "91", "2", "8/10", "0/10", "5/10", "19269", "80% x2, 78%", "the level", "", "../rowinile-Demonlist/img/starlight.png"),
     new Level("Starlit Stroll by Enfur", "https://youtu.be/w0UNwmrgOt8", "10", "84", "2", "9/10", "10/10", "8/10", "10935 (153 for rebeat)", "100%, 86%, 79%", "35-45, 60-68", "0-11, 68-76", "../rowinile-Demonlist/img/starlit.png"),
     new Level("Sky Shredder by dorvict", "https://youtu.be/Mn6JugEwMHk", "7", "73", "1", "10/10", "10/10", "9/10", "19753", "86%, 77%, 70%", "18-23, 27, 39-52, 70-86", "9-15, 52-70", "../rowinile-Demonlist/img/skyShredder.png"),
-    new Level("RUST by neigefeu", "2", "https://youtu.be/82rnpDKG45Q", "79", "1", "8/10", "7/10", "9/10", "17507", "95% x3, 94%, 88%, 86% x2", "0-17, 24-27, 76-100", "43-76", "../rowinile-Demonlist/img/rust.png"),
+    new Level("RUST by neigefeu", "https://youtu.be/82rnpDKG45Q", "2", "79", "1", "8/10", "7/10", "9/10", "17507", "95% x3, 94%, 88%, 86% x2", "0-17, 24-27, 76-100", "43-76", "../rowinile-Demonlist/img/rust.png"),
     new Level("Lucid Nightmares by CairoX", "https://youtu.be/Pof6tfPB3pA", "2", "79", "1", "6/10", "3/10", "5/10", "56244", "90%, 75% x4, 74% x2, 72% x3", "2-7, 50-66, 72-75", "40-50, 7-18", "../rowinile-Demonlist/img/lucidNightmares.png"),
-    new Level("The Rupture by Ka1ns", "5", "https://youtu.be/O90XpWzvoxQ", "103", "3", "7/10", "7/10", "7/10", "14690", "71% x3, 70%, 62% x2", "11-13, 34-36, 45-50, 69-72", "18-27, 30-34, 54-65, 81-100", "../rowinile-Demonlist/img/theRupture.png"),
-    new Level("Sazerix by Zeptrus", "3", "https://youtu.be/9BGKB-diLHM", "110", "2", "8/10", "9/10", "8/10", "10620", "76%, 71%, 70%", "17-32, 0-17 (in that order)", "50-66", "../rowinile-Demonlist/img/sazerix.png"),
+    new Level("The Rupture by Ka1ns", "https://youtu.be/O90XpWzvoxQ", "5", "103", "3", "7/10", "7/10", "7/10", "14690", "71% x3, 70%, 62% x2", "11-13, 34-36, 45-50, 69-72", "18-27, 30-34, 54-65, 81-100", "../rowinile-Demonlist/img/theRupture.png"),
+    new Level("Sazerix by Zeptrus", "https://youtu.be/9BGKB-diLHM", "3", "110", "2", "8/10", "9/10", "8/10", "10620", "76%, 71%, 70%", "17-32, 0-17 (in that order)", "50-66", "../rowinile-Demonlist/img/sazerix.png"),
     new Level("Endless Dream by DreamTide", "https://youtu.be/jGplXh3pZDo", "6", "N/A", "5", "10/10", "10/10", "10/10", "?", "50%, 49%", "0-23, 32-34, 85-100", "23-32, 36-65", "../rowinile-Demonlist/img/endlessDream.png"),
-    new Level("Bloodbath by Riot", "1", "https://youtu.be/t0yCY1tKMhU", "N/A", "1", "4/10", "5/10", "1/10", "6022+", "N/A", "N/A", "N/A", "../rowinile-Demonlist/img/bloodbath.png"),
-    new Level("Hidden in The Sand", "9", "#", "N/A", "9", "3/10", "5/10", "5/10", "N/A", "N/A", "N/A", "N/A", "../rowinile-Demonlist/img/hiddenInTheSand.png")
+    new Level("Bloodbath by Riot", "https://youtu.be/t0yCY1tKMhU", "1", "N/A", "1", "4/10", "5/10", "1/10", "6022+", "N/A", "N/A", "N/A", "../rowinile-Demonlist/img/bloodbath.png"),
+    new Level("Hidden in The Sand", "https://youtu.be/dQw4w9WgXcQ", "9", "N/A", "9", "3/10", "5/10", "5/10", "N/A", "N/A", "N/A", "N/A", "../rowinile-Demonlist/img/hiddenInTheSand.png")
 ]
 
 const displayLevel = (levels) => {
@@ -45,7 +45,7 @@ const displayLevel = (levels) => {
             </div>
             <div id="list-level-details-${Number(i + 1)}" class="col-12 list-level-details">
                 <span>
-                    Completion Video: <strong><a src="${levels[i].completionLink}"></a>${levels[i].completionLink}</strong>
+                    Completion Video: <strong><a href="${levels[i].completionLink}">Click Me</a></strong>
                 </span>
                 <span>
                     Extreme Demon: <strong>#${levels[i].number}</strong>
