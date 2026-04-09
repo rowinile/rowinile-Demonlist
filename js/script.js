ac81bd2a5ea40610e50019434e1c647e0ae87ff2
rowinile <fbberatgok757@gmail.com>
2026-04-09 13:07:45 +0300

    Refactor event listeners and remove redundant alert

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 const list = document.querySelector("#list-levels")
-const row = document.querySelector(".row")
 
-alert("this is not a serious project, just something I made for both fun and for improving keep that in mind!")
-
-for(i = 1; i <= 11; i++) {
+for(i = 1; i < levels.length + 1; i++) {
     const listLevelDetails = document.querySelector(`#list-level-details-${i}`)
+    const listLevel = document.getElementById(`${i}`)
+    const listLevels = document.querySelectorAll("#list-levels .list-level")
+    const allListLevelDetails = document.querySelectorAll("#list-levels .list-level-details")
 
     document.getElementById(`${i}`).addEventListener("click", () => {
-        if(listLevelDetails.classList.contains("show")) {
+        if(listLevel.classList.contains("active")) {
             listLevelDetails.classList.remove("show")
+            listLevel.classList.remove("active")
         } else {
+            listLevels.forEach((level) => {
+                level.classList.remove("active")
+            })
+            allListLevelDetails.forEach((levelDetails) => {
+                levelDetails.classList.remove("show")
+            })
+            listLevel.classList.add("active")
             listLevelDetails.classList.add("show")
         }
     })
 }
+
+alert("this is not a serious project, just something I made for both fun and for improving keep that in mind!")
 
 console.log("Hello Bro")
